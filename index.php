3895980fa087d91a21e38167e2d1c2c3bb9ea2cc
Pavao Piršljin <pavaopirsljin@gmail.com>
2018-11-20 19:23:10 +0100

    google clone

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Google Clone</title>
+</head>
+<body>
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="https://www.google.hr/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+        </div>
+        <div class="searchContainer">
+            <input class="searchBox" type="text" name="term">
+            <input class="searchButton" type="submit" value="Search">
+        </div>
+    </div>
+
+</body>
+</html>
